d56cab4ab9b04a095def94b2b612310dfcdc7ce6
Dhaval Mulashiya <dhaval.mulasheeya@shivlab.com>
2024-12-15 12:57:36 +0530

    added subscription api

--- src/pages/SubscriptionDetail.jsx
+++ src/pages/SubscriptionDetail.jsx
@@ -13,6 +13,31 @@ import {
 } from "../utils/Url";
 import SubscriptionHistoryTable from "../components/SubscriptionTable";
 import SubscriptionCard from "../components/card/SubscriptionCard";
+import { useUserActiveSubscription } from "../hooks/useUserActiveSubscription";
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Form,
+  Placeholder,
+  Row,
+} from "react-bootstrap";
+
+const Chip = ({ name, isActive }) => {
+  return (
+    <Badge
+      pill
+      bg={isActive ? "primary" : "secondary"} // Change color based on active state
+      className="px-3 py-2" // Add padding for chip-like appearance
+      style={{
+        cursor: "pointer",
+      }}
+    >
+      {name}
+    </Badge>
+  );
+};
 
 const SubscriptionPage = () => {
   const [subscriptions, setSubscriptions] = useState([]);
@@ -51,6 +76,31 @@ const SubscriptionPage = () => {
     fetchSubscriptions();
   }, []);
 
+  const {
+    data: userActiveSubscription,
+    isLoading: isLoadinUserActiveSubscription,
+    error: userActiveSubscriptionError,
+  } = useUserActiveSubscription();
+
+  console.log("userActiveSubscription", userActiveSubscription);
+
+  const {
+    subscription_status,
+    days_left,
+    package_name,
+    final_amount,
+    package_heading,
+    start_date,
+    end_date,
+    freeze_start_date,
+    freeze_end_date,
+    is_auto_renew,
+    subscription_id,
+    subscription_type,
+    show_to_merchant,
+    show_to_vendor,
+  } = userActiveSubscription?.data || {};
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -82,31 +132,116 @@ const SubscriptionPage = () => {
 
       <div className="row">
         {/* Current Subscription */}
-        <div className="col-md-4 mb-4">
-          <h5 className="card-title mb-2">Current Subscription</h5>
-          <div className="card shadow bg-dark text-light">
-            <div className="card-body">
-              <p className="fw-bold fs-4">45 days left</p>
-              <h2 className="fw-bold text-primary">£1283.24</h2>
-              <p className="text-muted">10/09/2024 18:49 - 11/09/2025 18:49</p>
-              <div className="form-check mb-3">
-                <input
+        {isLoadinUserActiveSubscription ? (
+          <div className="col-md-4 mb-4">
+            <h5 className="card-title mb-2">
+              <Placeholder as="span" animation="glow">
+                <Placeholder xs={6} />
+              </Placeholder>
+            </h5>
+            <Card className="shadow bg-dark text-light">
+              <Card.Body>
+                <Placeholder as="p" animation="glow">
+                  <Placeholder xs={4} className="fw-bold fs-4" />
+                </Placeholder>
+                <Placeholder as="h2" animation="glow">
+                  <Placeholder xs={5} className="fw-bold text-primary" />
+                </Placeholder>
+                <Placeholder as="p" animation="glow">
+                  <Placeholder xs={8} />
+                </Placeholder>
+                <Form.Check
                   type="checkbox"
-                  className="form-check-input"
-                  id="autoRenew"
+                  id="autoRenewSkeleton"
+                  disabled
+                  label={
+                    <Placeholder as="span" animation="glow">
+                      <Placeholder xs={6} />
+                    </Placeholder>
+                  }
                 />
-                <label className="form-check-label" htmlFor="autoRenew">
-                  Auto Renew
-                </label>
-              </div>
-              <div className="d-flex">
-                <button className="btn btn-success me-2">Renew</button>
-                <button className="btn btn-warning me-2">Freeze</button>
-                <button className="btn btn-danger">Terminate</button>
+                <div className="d-flex mt-3">
+                  {[1, 2, 3].map((key) => (
+                    <Button
+                      key={key}
+                      variant="secondary"
+                      disabled
+                      className="me-2"
+                    >
+                      <Placeholder as="span" animation="glow">
+                        <Placeholder xs={6} />
+                      </Placeholder>
+                    </Button>
+                  ))}
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+        ) : (
+          <div className="col-md-4 mb-4">
+            <h5 className="card-title mb-2">Current Subscription</h5>
+            <div className="card shadow bg-dark text-light">
+              <div className="card-body">
+                <p className="fw-bold fs-4">{days_left} days left</p>
+                <p className="fw-bold fs-4">{package_name}</p>
+                <h2 className="fw-bold text-primary mb-3">{final_amount}</h2>
+                <p className="">
+                  {start_date} - {end_date}
+                </p>
+                <p className="">Fronzen date - Unfrozen date</p>
+                <p className="">
+                  {freeze_start_date} - {freeze_end_date}
+                </p>
+                <div className="d-flex justify-content-between mb-3">
+                  <div className="form-check mb-3">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="autoRenew"
+                    />
+                    <label className="form-check-label" htmlFor="autoRenew">
+                      Auto Renew
+                    </label>
+                  </div>
+                  <Badge
+                    pill
+                    bg="primary" // Change color based on active state
+                    className="p-3" // Add padding for chip-like appearance
+                    style={{
+                      cursor: "pointer",
+                    }}
+                  >
+                    {subscription_status}
+                  </Badge>
+                </div>
+                <Row className="mb-2">
+                  <Col xs={6} className="d-flex justify-content-end pe-1">
+                    <Button variant="success" className="w-100">
+                      Renew
+                    </Button>
+                  </Col>
+                  <Col xs={6} className="d-flex justify-content-start ps-1">
+                    <Button variant="info" className="w-100">
+                      Freeze
+                    </Button>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={6} className="d-flex justify-content-end pe-1">
+                    <Button variant="warning" className="w-100">
+                      Cancel
+                    </Button>
+                  </Col>
+                  <Col xs={6} className="d-flex justify-content-start ps-1">
+                    <Button variant="danger" className="w-100">
+                      Terminate
+                    </Button>
+                  </Col>
+                </Row>
               </div>
             </div>
           </div>
-        </div>
+        )}
 
         {/* Next Subscription */}
         <div className="col-md-8 mb-4">
