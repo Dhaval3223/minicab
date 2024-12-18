b43b7dbacd27278f65a2ad31d8496e62f34bfd6a
Dhaval Mulashiya <dhaval.mulasheeya@shivlab.com>
2024-12-19 01:06:38 +0530

    added trsury and report tab

--- src/hooks/useTransactions.js
+++ src/hooks/useTransactions.js
@@ -1,21 +1,23 @@
 import { useQuery } from "react-query";
 import axiosInstance from "../utils/axiosInstance";
 
-const fetchTransactions = async (transactionMode) => {
-  const { data } = await axiosInstance.get(`/get-user-transaction-list`, {
-    params: { transaction_mode: transactionMode },
-  });
+const fetchUserTransactionList = async (params) => {
+  const { data } = await axiosInstance.get(
+    "https://minicab.124124.site/public/api/get-user-transaction-list",
+    { params }
+  );
   return data;
 };
 
-export const useTransactions = (transactionMode) => {
+const useUserTransactionList = (params, options = {}) => {
   return useQuery(
-    ["transactions", transactionMode],
-    () => fetchTransactions(transactionMode),
-    {
-      staleTime: 5 * 60 * 1000, // Cache data for 5 minutes
-      refetchOnWindowFocus: false, // Disable refetch on window focus
-      enabled: !!transactionMode, // Ensure query is enabled only when transactionMode is set
-    }
+    ["transactions", params],
+    () => fetchUserTransactionList(params),
+    // {
+    //   keepPreviousData: true,
+    //   ...options,
+    // }
   );
 };
+
+export default useUserTransactionList;
